6198bd41fc2080d99b2d860e200e7f348f75cda9
unknown <skander.saadaoui@esprit.tn>
2022-04-17 14:48:27 +0100

    first commit

---
+++ src/main/java/Personal/train/principal/PISYS/PiSysApplication.java
@@ -0,0 +1,13 @@
+package Personal.train.principal.PISYS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Personal/train/principal/PISYS/PiSysApplicationTests.java
@@ -0,0 +1,13 @@
+package Personal.train.principal.PISYS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
